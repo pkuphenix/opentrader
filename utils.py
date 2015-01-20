82808ec288615cc636be51609df96cfa3c67dd31
Li Qian <lqian@lqian.net>
2015-01-20 11:47:34 +0800

    added tixis initial files

--- utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-# str_time: "2010-06-04 21:08:12"
-def gen_time(str_time):
-    return datetime.strptime(str_time, "%Y-%m-%d %H:%M:%S")
-
-def gen_tick(str_time, precision=1):
-    return 
