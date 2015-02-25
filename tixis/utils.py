c29bd61caba0dd3981cff370c99d7dae6563fd85
qianli <pkuphenix@gmail.com>
2015-02-25 09:37:49 +0000

    added captcha hacker

---
+++ tixis/utils.py
@@ -0,0 +1,16 @@
+from tixis import app
+from flask import request, render_template, url_for, redirect, abort
+from bson.objectid import ObjectId
+from tixis.model import *
+from agents.xueqiu.api import XueqiuAPI
+
+@app.route("/utils/captcha", methods=['POST','GET'])
+def captcha_hack():
+    a = XueqiuAPI()
+    if request.method == 'GET':
+        a.captcha_get('/var/www/test/captcha.jpg')
+        return render_template('captcha_hack.html', error_show='hidden')
+    elif request.method == 'POST':
+        captcha = request.form['captcha']
+        e = a.captcha_post(captcha)
+        return render_template('captcha_hack.html', error_show='', error_msg=str(e))
